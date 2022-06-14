83d7d9d779c5f1d99777da5384a9a551aa519ed1
Shaul Xu <302763630@qq.com>
2022-06-14 10:42:38 +0800

    two sum

--- 0001/TwoSum.ts
+++ 0001/TwoSum.ts
@@ -1,3 +1,11 @@
 function twoSum(nums: number[], target: number): number[] {
-  // TODO
+  const map = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    const cur = nums[i];
+    const result = target - cur;
+    if (map.has(result)) {
+      return [map.get(result), i];
+    }
+    map.set(cur, i);
+  }
 }
