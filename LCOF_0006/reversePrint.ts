0ab5375ddaedfda12ae5f14581daa523c19d1622
Shaul Xu <302763630@qq.com>
2022-06-15 15:07:55 +0800

    reverse print

---
+++ LCOF_0006/reversePrint.ts
@@ -0,0 +1,12 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function reversePrint(head: ListNode | null): number[] {
+  return head === null ? [] : reversePrint(head.next).concat(head.val);
+}
